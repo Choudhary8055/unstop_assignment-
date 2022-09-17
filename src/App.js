17f0b93945061c3d8ac1a75add9bf6d84d0120b8
Choudhary8055 <rahulchoudhary200599@gmail.com>
2022-09-17 13:57:47 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+import Train from "./components/Train";
+import ShowCase from "./components/ShowCase";
+import ShowSeat from "./components/ShowSeat";
+const trains = [
+  {
+    name: "Rajdhani Express",
+    price: 200,
+    occupied: [20, 21, 30, 1, 2, 8]
+  },
+  {
+    name: "Humsafar Express",
+    price: 120,
+    occupied: [9, 41, 35, 11, 65, 26]
+  },
+  {
+    name: "Duronto Express",
+    price: 180,
+    occupied: [37, 25, 44, 13, 2, 3]
+  },
+  {
+    name: "Sampark Kranti Express",
+    price: 290,
+    occupied: [10, 12, 50, 33, 28, 47]
+  }
+];
 
-function App() {
+const seats = Array.from({ length: 10 * 8 }, (_, i) => i);
+export default function App() {
+  const [selectedTrain, setSelectedTrain] = useState(trains[0]);
+  const [selectedSeats, setSelectedSeats] = useState([]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Train
+        train={selectedTrain}
+        trains={trains}
+        onChange={(train) => {
+          setSelectedSeats([]);
+          setSelectedTrain(train);
+        }}
+      />
+      <ShowCase />
+
+      <ShowSeat
+        train={selectedTrain}
+        seats={seats}
+        selectedSeats={selectedSeats}
+        onSelectedSeatsChange={(selectedSeats) =>
+          setSelectedSeats(selectedSeats)
+        }
+      />
+      <p className="info">
+        You have selected <span className="count">{selectedSeats.length}</span>{" "}
+        seats for the price of{" "}
+        <span className="total">
+          {selectedSeats.length * selectedTrain.price}₹
+        </span>
+      </p>
     </div>
   );
 }
-
-export default App;
---
+++ src/components/ShowSeat.js
@@ -0,0 +1,49 @@
+import React from "react";
+import clsx from "clsx";
+import "../App.css";
+
+function ShowTrain({ train, seats, selectedSeats, onSelectedSeatsChange }) {
+  function handleSelectedState(seat) {
+    const isSelected = selectedSeats.includes(seat);
+    if (isSelected) {
+      onSelectedSeatsChange(
+        selectedSeats.filter((selectedSeat) => selectedSeat !== seat)
+      );
+    } else {
+      onSelectedSeatsChange([...selectedSeats, seat]);
+    }
+  }
+
+  return (
+    <div className="trainSeat">
+      <div className="seats">
+        {seats.map((seat) => {
+          const isSelected = selectedSeats.includes(seat);
+          const isOccupied = train.occupied.includes(seat);
+          return (
+            <span
+              tabIndex="0"
+              key={seat}
+              className={clsx(
+                "seat",
+                isSelected && "selected",
+                isOccupied && "occupied"
+              )}
+              onClick={isOccupied ? null : () => handleSelectedState(seat)}
+              onKeyPress={
+                isOccupied
+                  ? null
+                  : (e) => {
+                      if (e.key === "Enter") {
+                        handleSelectedState(seat);
+                      }
+                    }
+              }
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+export default ShowTrain;
